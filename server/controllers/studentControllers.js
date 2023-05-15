7bec5e5ac0849fb1fb703be6cfe3106a22d28c3e
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-05-15 19:03:56 -0300

    Modifica os controladores de get e post

--- server/controllers/studentControllers.js
+++ server/controllers/studentControllers.js
@@ -1,27 +1,57 @@
 const asyncHandler = require("express-async-handler");
-const { BadRequestError } = require("../middlewares/httpErrors");
+const Students = require("../models/studentsSchema");
+const { isMongoId } = require("validator");
+const {
+    BadRequestError,
+    InternalServerError,
+    NotFoundError,
+} = require("../middlewares/httpErrors");
 
 const getAllStudents = asyncHandler(async (req, res) => {
-    res.json({ message: `Aqui vai todos os estudantes` });
+    // Limitando a 300 estudantes
+    const allStudents = await Students.find().limit(300);
+    res.json(allStudents);
 });
 
 const getStudentById = asyncHandler(async (req, res) => {
-    res.json({ message: `Aqui vai o estudante de id: ${req.params.id}` });
+    if (!isMongoId(req.params.id)) {
+        throw new BadRequestError("MongoId inválido.");
+    }
+    const student = await Students.findById(req.params.id);
+    if (!student) {
+        throw new NotFoundError("Estudante não encontrado.");
+    }
+    res.json(student);
 });
 
 const postStudent = asyncHandler(async (req, res) => {
-    const { name, email } = req.body;
-    if (!name || !email) {
-        throw new BadRequestError("O campo de nome e email são necessários.");
-    } else {
-        res.json({
-            message: `Aqui cria um novo cadastro com: ${JSON.stringify(
-                req.body
-            )}`,
-        });
+    const existingStudent = await Students.findOne({ email: req.body.email });
+    if (existingStudent) {
+        throw new BadRequestError("Email já está em uso.");
+    }
+    try {
+        const newStudent = new Students(req.body);
+        const savedStudent = await newStudent.save();
+        res.status(201).json(savedStudent);
+    } catch (err) {
+        // Trata os erros lançados pelo mongoose e passa para nosso middleware
+        if (err.name === "ValidationError") {
+            const nestedErrorsArray = Object.values(err.errors);
+            const errorMessagesArray = nestedErrorsArray.map((e) => {
+                if (e.name === "CastError") {
+                    return `Tipo inválido para o campo ${e.path}`;
+                }
+                return e.message;
+            });
+            throw new BadRequestError(errorMessagesArray.join("; ") + ".");
+        } else {
+            throw new InternalServerError("Erro interno do servidor.");
+        }
     }
 });
 
+// Vou deixar para implementar os controladores de editar
+// e deletar depois de implementar autenticação e registro.
 const putStudent = asyncHandler(async (req, res) => {
     res.json({
         message: `Aqui atualiza um cadastro com: ${JSON.stringify(req.body)}`,
