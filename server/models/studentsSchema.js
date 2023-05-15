3704941748b6ad57cf04b0c46520fa39f72c5cb4
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-05-15 17:37:25 -0300

    Modifica schema para garantir validação dos campos

--- server/models/studentsSchema.js
+++ server/models/studentsSchema.js
@@ -1,48 +1,50 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { isEmail, isStrongPassword } = require("validator");
 const { DEGREES, GENDERS } = require("../../constants.js");
 
 const StudentSchema = new mongoose.Schema(
     {
         fullname: {
             type: String,
-            required: true,
+            required: [true, "Falta o campo nome completo"],
         },
         email: {
             type: String,
-            required: true,
+            required: [true, "Falta o campo email"],
             unique: true,
+            validate: {
+                validator: isEmail,
+                message: "Email inválido",
+            },
         },
         password: {
             type: String,
-            required: true,
+            required: [true, "Falta o campo senha"],
+            validate: {
+                validator: isStrongPassword,
+                message: "Senha fraca",
+            },
         },
         dateOfBirth: {
             type: Date,
-            required: true,
+            required: [true, "Falta o campo data de nascimento"],
         },
         gender: {
             type: String,
-            required: true,
-            enum: GENDERS,
+            required: [true, "Falta o campo gênero"],
+            enum: {
+                values: GENDERS,
+                message: "Gênero inválido",
+            },
         },
-        // customGender: {
-        //     type: String,
-        //     validade: {
-        //         validator: (value) => {
-        //             if (this.gender !== "Outro" && value) {
-        //                 return false;
-        //             }
-        //             if (this.gender === "Outro" && !value) {
-        //                 return false;
-        //             }
-        //             return true;
-        //         },
-        //     },
-        // },
         degree: {
             type: String,
-            required: true,
-            enum: DEGREES,
+            required: [true, "Falta o campo curso"],
+            enum: {
+                values: DEGREES,
+                message: "Curso inválido",
+            },
         },
         isAdmin: {
             type: Boolean,
@@ -51,5 +53,19 @@ const StudentSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+StudentSchema.pre("save", async function (next) {
+    if (this.isModified("password") || this.isNew) {
+        try {
+            const hash = await bcrypt.hash(this.password, 10);
+            this.password = hash;
+            next();
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        next();
+    }
+});
 
 module.exports = mongoose.model("Students", StudentSchema);
