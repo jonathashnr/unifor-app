60886fc1fd7338a95819b80aa2492ac934d7a53a
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-05-16 12:00:03 -0300

    Implementa rota de login com jsonwebtoken

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,14 +1,47 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {
+    BadRequestError,
+    UnauthorizedError,
+} = require("../middlewares/httpErrors");
+const Students = require("../models/studentsSchema.js");
 
 const registerUser = asyncHandler(async (req, res) => {
     res.json({ message: "Registra um novo estudante com status de admin" });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Loga e autentica um estudante." });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError(
+            `Campo de ${email ? "senha" : "email"} vazio.`
+        );
+    }
+    const user = await Students.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "15m" }
+        );
+        res.json({ accessToken });
+    } else {
+        throw new UnauthorizedError("Email ou senha inválidos.");
+    }
+});
+
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Info sobre o usuário logado." });
 });
 
 module.exports = {
     registerUser,
     loginUser,
+    currentUser,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
     registerUser,
     loginUser,
+    currentUser,
 } = require("../controllers/userControllers.js");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/current", currentUser);
 
 module.exports = router;
