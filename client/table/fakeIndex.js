358920a8d8c304498863d818c3c40fbbd26b438c
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-05-24 12:02:38 -0300

    Refatora os modais e implementa tratamento de erros base no form de registro

--- client/table/fakeIndex.js
+++ client/table/fakeIndex.js
@@ -1,28 +1,24 @@
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
-const baseURI = "http://localhost:3000/admin";
 
-// Testing
-const loginModalObj = new bootstrap.Modal(
-    document.getElementById("loginModal"),
-    {}
-);
-const registerModalObj = new bootstrap.Modal(
-    document.getElementById("registerModal"),
-    {}
-);
-const testLoginButton = document.getElementById("testLoginButton");
-testLoginButton.addEventListener("click", (e) => {
-    loginModalObj.hide();
-});
+// Define os modais que podem ser manipulados via js
+const loginModalObj = new bootstrap.Modal("#loginModal", {});
+const registerModalObj = new bootstrap.Modal("#registerModal", {});
+const errModalObj = new bootstrap.Modal("#errModal", {});
+const dialogModalObj = new bootstrap.Modal("#dialogModal", {});
 
+// Função que monta um modal para uma mensagem de erro
+// especifica, "target" é o alvo do retorno para uma nova
+// tentativa
 const showErrorDialog = (target, title, message) => {
-    const button = document.getElementById("errButton");
     document.getElementById("errModalLabel").innerText = title;
     document.getElementById("errMessage").innerText = message;
     document.getElementById("errModalButton").dataset.bsTarget = target;
-    button.click();
+    errModalObj.show();
 };
+
+// Helper para URI
+const endpoint = (route) => `http://localhost:3000/admin/${route}`;
 
 loginForm.addEventListener(
     "submit",
@@ -30,9 +26,8 @@ loginForm.addEventListener(
         event.preventDefault();
         const { email, password } = loginForm.elements;
         const body = { email: email.value, password: password.value };
-        console.log(baseURI + "/login", body);
         axios
-            .post(baseURI + "/login", body)
+            .post(endpoint("login"), body)
             .then((res) => {
                 localStorage.setItem("jwtToken", res.data.accessToken);
                 window.location.href = "table.html";
@@ -40,6 +35,7 @@ loginForm.addEventListener(
             .catch((err) => {
                 const title = `${err.response.status} - ${err.response.data.title}`;
                 const message = err.response.data.message;
+                loginModalObj.hide();
                 showErrorDialog("#loginModal", title, message);
             });
     },
@@ -50,54 +46,28 @@ registerForm.addEventListener(
     "submit",
     (event) => {
         event.preventDefault();
-        const { email, password } = registerForm.elements;
-        const body = { email: email.value, password: password.value };
-        axios
-            .post(baseURI + "/register", body)
-            .then((res) => {
-                registerModalObj.hide();
-                loginModalObj.show();
-                // document.getElementById("loginInputEmail").value = email.value;
-                // document.getElementById("loginInputPassword").value =
-                //     password.value;
-                // const button = document.getElementById("loginButton");
-                // button.click();
-            })
-            .catch((err) => {
-                const title = `${err.response.status} - ${err.response.data.title}`;
-                const message = err.response.data.message;
-                showErrorDialog("#registerModal", title, message);
-            });
+        event.stopPropagation();
+        registerForm.classList.add("was-validated");
+        if (registerForm.checkValidity()) {
+            const { email, password } = registerForm.elements;
+            const body = {
+                email: email.value,
+                password: password.value,
+            };
+            axios
+                .post(endpoint("register"), body)
+                .then((res) => {
+                    registerModalObj.hide();
+                    dialogModalObj.show();
+                })
+                .catch((err) => {
+                    event.stopPropagation();
+                    const title = `${err.response.status} - ${err.response.data.title}`;
+                    const message = err.response.data.message;
+                    registerModalObj.hide();
+                    showErrorDialog("#registerModal", title, message);
+                });
+        }
     },
     false
 );
-// // Selecionar os formulários de login e registro pelos IDs dos modais
-// let loginForm = document.querySelector("#loginModal form");
-// let registerForm = document.querySelector("#registerModal form");
-// let button = document.querySelector("#loginForm button");
-// console.log("Entrou no modulo");
-// console.log(loginForm);
-// // Função para lidar com a submissão do formulário
-// function formSubmitHandler(e) {
-//     e.preventDefault(); // Evitar o comportamento padrão de submissão do formulário
-//     console.log("entrou na funcao");
-//     let form = e.target;
-//     let data = {};
-
-//     // Loop em cada elemento do formulário para coletar os dados
-//     for (let i = 0; i < form.length; i++) {
-//         let input = form[i];
-//         // Ignorar botões e outros tipos de input não-necessários
-//         if (input.type === "submit" || input.type === "button" || !input.id)
-//             continue;
-
-//         data[input.id] = input.value;
-//     }
-
-//     // Fazendo log dos dados no console
-//     console.log(data);
-// }
-
-// // Adicionar os manipuladores de eventos de submissão
-// loginForm.addEventListener("submit", formSubmitHandler);
-// registerForm.addEventListener("submit", formSubmitHandler);
