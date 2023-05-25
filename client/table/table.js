fd2b0c03c5c6fb99d8812aca379f52ed33ded8c1
Vitoria-m <vitoriazzz@outlook.com>
2023-05-25 15:25:18 -0300

    lógica de exclusão de usuário finalizada

--- client/table/table.js
+++ client/table/table.js
@@ -1,4 +1,4 @@
-const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiam9hb0BvdXRsb29rLmNvbSIsImlkIjoiNjQ2NTQ5MWJjZjI5YTc5MjRmZmUwZjk2In0sImlhdCI6MTY4NDc4MDU0NiwiZXhwIjoxNjg0ODcwNTQ2fQ.HESeG5z55qSVGHHPhAJMuP2lNN3fkfVLepOTs3psMRM"
+const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiam9hb0BvdXRsb29rLmNvbSIsImlkIjoiNjQ2NTQ5MWJjZjI5YTc5MjRmZmUwZjk2In0sImlhdCI6MTY4NDk2MzMzMiwiZXhwIjoxNjg1MDUzMzMyfQ.OJNOV6vWj5PZ2ysPO7eLPnT50y5ugAjALvGa3fKvY6Q"
 const url = `http://localhost:3000/student`   
 const authAxios = axios.create({
     baseURL: url,
@@ -7,6 +7,7 @@ const authAxios = axios.create({
     }
 })
 let currentData = 20
+let IDItem
 
 function displayStudents() {
     authAxios.get(url).then((res) => {
@@ -31,20 +32,31 @@ function displayStudents() {
                 <td class='dataCell'>${data[i].gender}</td>
                 <td class='dataCell'>${data[i].degree}</td> 
                 <td class='dataCell'>Editar</td> 
-                <td class='dataCell'>Deletar</td> 
+                <td class='dataCell'>
+                    <i class='fa-solid fa-trash deleteIcon' onclick=openConfirmationModal() id=${data[i]._id}></i>
+                </td> 
             `
             document.querySelector('tbody').insertAdjacentHTML('beforeend', content)
          } 
     }) 
     .catch((error) => {
-        console.log(error)
+        if (error.response) {
+            console.log(error.response.data)
+            console.log(error.response.status)
+            console.log(error.response.headers)
+        } else if (error.request) {
+            console.log(error.request)
+        } else {
+            console.log('Error', error.message)
+        }
+        console.log(error.config)
     })  
 }
 displayStudents()
 
 function loadMore() { 
     let bodyRow = [...document.querySelectorAll('tbody tr')]
-    var endTable = currentData + 20
+    let endTable = currentData + 20
 
     if(endTable >= bodyRow.length) {
         endTable = bodyRow.length
@@ -55,4 +67,36 @@ function loadMore() {
         bodyRow[i].style.display = 'table-row'
     }   
     currentData = endTable 
+}
+
+function openConfirmationModal() {
+    document.getElementById('dialog').showModal()
+}
+
+function closeConfirmationModal() {
+    document.getElementById('dialog').close()
+}
+
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('deleteIcon')) {
+        IDItem = e.target.id            
+    }   
+})
+
+function deleteItem() {
+    authAxios.delete(`${url}/${IDItem}`).then(() => {
+        location.reload();          
+    })
+    .catch((error) => {
+        if (error.response) {
+                console.log(error.response.data)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+            } else if (error.request) {
+                console.log(error.request)
+            } else {
+                console.log('Error', error.message)
+            }
+            console.log(error.config)
+    })
 }
